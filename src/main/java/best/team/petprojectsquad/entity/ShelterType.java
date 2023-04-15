004bc0ff0b52641f8ea70ccca608f1b0517e6d09
denispetko <den.petko@rambler.ru>
2023-04-15 21:15:59 +0300

    исправлен shalter

--- src/main/java/best/team/petprojectsquad/entity/ShelterType.java
+++ src/main/java/best/team/petprojectsquad/entity/ShelterType.java
@@ -1,8 +1,8 @@
 package best.team.petprojectsquad.entity;
 
 public enum ShelterType {
-    CAT_SHELTER("/catShalter"),
-    DOG_SHELTER("/dogShalter");
+    CAT_SHELTER("/catShelter"),
+    DOG_SHELTER("/dogShelter");
     private final String shelterType;
 
     ShelterType(String shelterType) {
