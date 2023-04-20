3476e1201d1ad98605a2cff7543e979a8a759cb1
egorcouldlie <couldliegor@gmail.com>
2023-04-20 15:49:35 +0300

    Tests 0.5

--- src/test/java/best/team/petprojectsquad/controller/CatControllerTest.java
+++ src/test/java/best/team/petprojectsquad/controller/CatControllerTest.java
@@ -2,38 +2,56 @@ package best.team.petprojectsquad.controller;
 
 import best.team.petprojectsquad.entity.Cat;
 import best.team.petprojectsquad.repository.CatRepository;
+import org.hibernate.LazyInitializationException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.contains;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(CatController.class)
 @SpringBootTest
-class CatControllerTest{
-    private Cat cat = new Cat(0, "ВОт", "Test,you may delete this", 2000, "Описание");
+class CatControllerTest {
+    private Cat cat = new Cat(0, "ВОт", "Test", 2000, "Описание");
     private Long id = 0L;
-
     @Autowired
-    CatController catController;
+    private MockMvc mockMvc;
+    @Autowired
+    CatRepository catRepository;
 
     @Test
-    void testingGetter() {
-        catController.addCat(cat);
-        ResponseEntity<Cat> catById = catController.getCatById(0);
-        assertEquals(catById.getBody(), cat);
-    }
-    @Test
-    public void contextLoads() {
-        assertThat(catController).isNotNull();
+    void testingGetter() throws Exception {
+        catRepository.save(cat);
+        this.mockMvc.perform(get("/1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(String.valueOf(cat))));
+
     }
 }
