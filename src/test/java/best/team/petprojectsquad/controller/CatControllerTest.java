f683b6b88330092be1813ffeecc2f143e495db30
egorcouldlie <couldliegor@gmail.com>
2023-04-20 14:34:11 +0300

    Tests 0.4

--- src/test/java/best/team/petprojectsquad/controller/CatControllerTest.java
+++ src/test/java/best/team/petprojectsquad/controller/CatControllerTest.java
@@ -7,26 +7,33 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class CatControllerTest{
-    private Cat cat = new Cat(0, "ВОт", "Name", 2000, "Описание");
+    private Cat cat = new Cat(0, "ВОт", "Test,you may delete this", 2000, "Описание");
     private Long id = 0L;
 
-    @Mock
+    @Autowired
     CatController catController;
 
     @Test
     void testingGetter() {
         catController.addCat(cat);
         ResponseEntity<Cat> catById = catController.getCatById(0);
-        System.out.println("Good");
+        assertEquals(catById.getBody(), cat);
+    }
+    @Test
+    public void contextLoads() {
+        assertThat(catController).isNotNull();
     }
 }
