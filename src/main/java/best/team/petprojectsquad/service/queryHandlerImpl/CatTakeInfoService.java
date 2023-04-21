e12681ec574343f7490c5fd8211f331c0ae7ca19
denispetko <den.petko@rambler.ru>
2023-04-21 16:51:26 +0300

    add takeInfo services

---
+++ src/main/java/best/team/petprojectsquad/service/queryHandlerImpl/CatTakeInfoService.java
@@ -0,0 +1,54 @@
+package best.team.petprojectsquad.service.queryHandlerImpl;
+
+import best.team.petprojectsquad.service.QueryHandlerService;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.Keyboard;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.SendMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatTakeInfoService implements QueryHandlerService {
+    @Override
+    public List<BaseRequest> getReplyMessage(long id) {
+        List<BaseRequest> requestArrayList = new ArrayList<>();
+        SendMessage sendMessage = new SendMessage(id, """
+                Кошачий приют приветствует вас. Ниже представлена информация, которая поможет Вам разобраться
+                с бюрократическими (оформление договора)
+                и бытовыми (как подготовиться к жизни с животным) вопросами:
+                """);
+        Keyboard keyboard = getTakeInfoCatMenuKeyboard();
+        sendMessage.replyMarkup(keyboard);
+        requestArrayList.add(sendMessage);
+
+        return requestArrayList;
+    }
+
+    private Keyboard getTakeInfoCatMenuKeyboard() {
+
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                new InlineKeyboardButton[][]{
+                        {new InlineKeyboardButton("правила знакомства с животным до того, как забрать его из приюта")
+                                .callbackData("/rulesBeforeGettingCat")},
+                        {new InlineKeyboardButton("список документов, необходимых для того, чтобы взять животное из приюта")
+                                .callbackData("/listOfDocuments")},
+                        {new InlineKeyboardButton("список рекомендаций по транспортировке животного")
+                                .callbackData("/transportRecommendations")},
+                        {new InlineKeyboardButton("список рекомендаций по обустройству дома для котенка")
+                                .callbackData("/homeRecommendationsKitten")},
+                        {new InlineKeyboardButton("список рекомендаций по обустройству дома для взрослого животного")
+                                .callbackData("/homeRecommendationsAdultCat")},
+                        {new InlineKeyboardButton("список рекомендаций по обустройству дома для для животного с " +
+                                "ограниченными возможностями").callbackData("/homeRecommendationsCatWithDisabilities")},
+                        {new InlineKeyboardButton("список причин, почему могут отказать и не дать забрать животное из приюта")
+                                .callbackData("/listReasonsNegative")},
+                        {new InlineKeyboardButton("записать контактные данные для связи")
+                                .callbackData("/sendContact")},
+                        {new InlineKeyboardButton("связаться с волонтером").callbackData("/callVolunteer")}
+                });
+
+        return inlineKeyboard;
+    }
+}
