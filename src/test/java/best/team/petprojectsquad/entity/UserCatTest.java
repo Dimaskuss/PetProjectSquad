b354e799cb39335ef5ab8daf464b8e6454db9e12
Дмитрий <dimadrumfire@gmail.com>
2023-04-21 21:42:25 +0600

    Entity Tests

---
+++ src/test/java/best/team/petprojectsquad/entity/UserCatTest.java
@@ -0,0 +1,26 @@
+package best.team.petprojectsquad.entity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserCatTest {
+
+    @Test
+    public void testGettersAndSetters() {
+        UserCat userCat = new UserCat();
+        userCat.setId(1);
+        userCat.setChatId(123456789);
+        userCat.setPhoneNumber(5551234);
+
+        assertEquals(1, userCat.getId());
+        assertEquals(123456789, userCat.getChatId());
+        assertEquals(5551234, userCat.getPhoneNumber());
+    }
+
+    @Test
+    public void testConstructor() {
+        UserCat userCat = new UserCat(123456789);
+
+        assertEquals(123456789, userCat.getChatId());
+    }
+}
