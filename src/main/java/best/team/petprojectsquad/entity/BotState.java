3d32cb38c35ea0c3502013318675a717334c476d
denispetko <den.petko@rambler.ru>
2023-04-24 13:21:52 +0300

    add handlers, services, tests - Stage 2

--- src/main/java/best/team/petprojectsquad/entity/BotState.java
+++ src/main/java/best/team/petprojectsquad/entity/BotState.java
@@ -17,8 +17,30 @@ public enum BotState {
     DOG_SHELTER_PASS("/dogPass"),
     CAT_SHELTER_SAFETY("/catSafety"),
     DOG_SHELTER_SAFETY("/dogSafety"),
-    SHELTER_CALLBACK("/callback");
+    SHELTER_CALLBACK("/callback"),
 
+    CAT_SHELTER_CALLBACK("/catCallback"),
+    DOG_SHELTER_CALLBACK("/dogCallback"),
+    CAT_TAKE_INFO("/catTakeInfo"),
+    DOG_TAKE_INFO("/dogTakeInfo"),
+
+    RULES_TAKE_CAT("/rulesBeforeGettingCat"),
+    RULES_TAKE_DOG("/rulesBeforeGettingDog"),
+    CAT_TAKE_DOCUMENTS("/catListOfDocuments"),
+    DOG_TAKE_DOCUMENTS("/dogListOfDocuments"),
+    CAT_TRANSPORT_RECOMMENDATION("/catTransportRecommendations"),
+    DOG_TRANSPORT_RECOMMENDATION("/dogTransportRecommendations"),
+    HOME_RECOMMENDATION_KITTEN("/homeRecommendationsKitten"),
+    HOME_RECOMMENDATION_PUPPY("/homeRecommendationsPuppy"),
+    HOME_RECOMMENDATION_ADULT_CAT("/homeRecommendationsAdultCat"),
+    HOME_RECOMMENDATION_ADULT_DOG("/homeRecommendationsAdultDog"),
+    HOME_RECOMMENDATION_DIS_CAT("/homeRecommendationsCatWithDisabilities"),
+    HOME_RECOMMENDATION_DIS_DOG("/homeRecommendationsDogWithDisabilities"),
+    DOG_TIPS_TRAINER("/tipsDogTrainer"),
+    VERIFIED_TRAINERS("/listVerifiedDogTrainers"),
+    DOG_TAKE_NEGATIVE("/dogListReasonsNegative"),
+    CONTACTS_CAT("/catSendContact"),
+    CONTACTS_DOG("/dogSendContact");
 
 
     private final String state;
