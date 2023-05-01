5e3844c0c945fcfb0bb9e12942f64f18df23199d
Viktor <viktor_23_08@mail.ru>
2023-05-01 19:47:18 +0200

    Add User to UserCat and UserDog Add functional CallBack for two shelters Add method to UserCatRepository and UserDogRepository

--- src/main/java/best/team/petprojectsquad/service/textHandlerImpl/DogValidatePhoneService.java
+++ src/main/java/best/team/petprojectsquad/service/textHandlerImpl/DogValidatePhoneService.java
@@ -1,10 +1,11 @@
 package best.team.petprojectsquad.service.textHandlerImpl;
 
 import best.team.petprojectsquad.Cache.UserDataCache;
-import best.team.petprojectsquad.entity.BotState;
-import best.team.petprojectsquad.entity.UserFeedBack;
-import best.team.petprojectsquad.repository.UserFeedBackRepository;
+import best.team.petprojectsquad.entity.*;
 import best.team.petprojectsquad.service.TextHandlerService;
+import best.team.petprojectsquad.service.ValidatePhone;
+import best.team.petprojectsquad.service.repositoryServiceImpl.UserDogServiceImpl;
+import best.team.petprojectsquad.service.repositoryServiceImpl.UserServiceImpl;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
@@ -14,35 +15,32 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Service
 @AllArgsConstructor
-public class ValidatePhoneService implements TextHandlerService {
+public class DogValidatePhoneService implements TextHandlerService, ValidatePhone {
+
     private final UserDataCache userDataCache;
-    private UserFeedBackRepository userFeedBackRepository;
+    private final UserServiceImpl userService;
+    private final UserDogServiceImpl userDogService;
 
     @Override
     public List<BaseRequest> getReplyMessage(Message message) {
         List<BaseRequest> requestArrayList = new ArrayList<>();
         if (checkPhone(message.text())) {
-            if (!userFeedBackRepository.existsByChatId(message.chat().id())) {
-                userFeedBackRepository.save(new UserFeedBack(message.text(), message.chat().id(), message.from().username(), "Dog"));
+            if (userDogService.getByUser(userService.get(message.chat().id()).get()).isEmpty()){
+                UserDog userDog = new UserDog();
+                userDog.setUserNeedHelp(UserNeedHelp.YES);
+                userDog.setUser(userService.get(message.chat().id()).get());
+                userDog.setPhoneNumber(message.text());
             }
             SendMessage sendMessage = new SendMessage(message.chat().id(), "Волонтер в ближайшее время Вам перезвонит.");
             requestArrayList.add(sendMessage);
             userDataCache.setUsersCurrentBotState(message.chat().id(),BotState.START);
-
         } else {
             SendMessage sendMessage = new SendMessage(message.chat().id(), "Телефон написан некорректно, пришлите еще раз в формате +79315556677");
             requestArrayList.add(sendMessage);
-
         }
-
         return requestArrayList;
     }
 
-    public boolean checkPhone(String phone) {
-        String regex = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$";
-        return phone.matches(regex);
-    }
 }
