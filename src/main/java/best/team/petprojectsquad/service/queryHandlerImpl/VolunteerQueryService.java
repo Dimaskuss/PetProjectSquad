39cf8a217e9b4aaf2b89f36c58de7f61d4287c13
Viktor <viktor_23_08@mail.ru>
2023-05-04 10:25:57 +0200

    fix bag call volonter info menu

--- src/main/java/best/team/petprojectsquad/service/queryHandlerImpl/VolunteerQueryService.java
+++ src/main/java/best/team/petprojectsquad/service/queryHandlerImpl/VolunteerQueryService.java
@@ -2,28 +2,36 @@ package best.team.petprojectsquad.service.queryHandlerImpl;
 
 import best.team.petprojectsquad.entity.User;
 import best.team.petprojectsquad.entity.UserFeedBack;
-import best.team.petprojectsquad.repository.UserFeedBackRepository;
-import best.team.petprojectsquad.repository.UserRepository;
 import best.team.petprojectsquad.service.QueryHandlerService;
+import best.team.petprojectsquad.service.repositoryServiceImpl.UserFeedBackService;
+import best.team.petprojectsquad.service.repositoryServiceImpl.UserServiceImpl;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class VolunteerQueryService implements QueryHandlerService {
-    private UserRepository userRepository;
-    private UserFeedBackRepository userFeedBackRepository;
+
+    private UserServiceImpl userRepositoryService;
+    private UserFeedBackService userFeedBackRepositoryService;
+
     @Override
     public List<BaseRequest> getReplyMessage(long id) {
         List<BaseRequest> requestArrayList = new ArrayList<>();
-        if (!userFeedBackRepository.existsByChatId(id)) {
-            User user = userRepository.getUserFeedBackByChatId(id);
-            userFeedBackRepository.save(new UserFeedBack(id, user.getName()));
+        if (!userFeedBackRepositoryService.existsByChatId(id)) {
+            try {
+                User user = userRepositoryService.findByChatId(id).orElseThrow(NullPointerException::new);
+                userFeedBackRepositoryService.save(new UserFeedBack(id, user.getName()));
+            }catch (NullPointerException e){
+                log.error("Error in class VolunteerQueryService" + e.getMessage(), e);
+            }
         }
         SendMessage sendMessage = new SendMessage(id, "Ваш запрос успешно зарегистрирован, ожидайте обратной связи!");
         requestArrayList.add(sendMessage);
