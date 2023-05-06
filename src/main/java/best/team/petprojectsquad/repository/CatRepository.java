fd1353be0495dfa96b14928ee4338cc788f1bf55
Viktor <119399653+Viktor2308@users.noreply.github.com>
2023-05-06 08:11:54 +0200

    Delete src/main directory

--- src/main/java/best/team/petprojectsquad/repository/CatRepository.java
+++
@@ -1,8 +0,0 @@
-package best.team.petprojectsquad.repository;
-
-import best.team.petprojectsquad.entity.Cat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatRepository extends JpaRepository<Cat,Long> {
-
-}
--- src/main/java/best/team/petprojectsquad/repository/DogRepository.java
+++
@@ -1,8 +0,0 @@
-package best.team.petprojectsquad.repository;
-
-import best.team.petprojectsquad.entity.Dog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DogRepository extends JpaRepository<Dog,Long> {
-
-}
