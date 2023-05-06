5aedba00ca15195c59330eca4ec7a4045818871d
denispetko <den.petko@rambler.ru>
2023-05-06 17:18:07 +0300

    add handlers, services, tests - Stage 2

---
+++ src/test/java/best/team/petprojectsquad/handler/MainHandlerTest.java
@@ -0,0 +1,86 @@
+package best.team.petprojectsquad.handler;
+
+import best.team.petprojectsquad.Cache.UserDataCache;
+import best.team.petprojectsquad.entity.BotState;
+import best.team.petprojectsquad.listener.TelegramBotUpdateListenerTest;
+import com.pengrad.telegrambot.BotUtils;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SendPhoto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MainHandlerTest {
+    @Mock
+    private BotStateContext botStateContext;
+    @Mock
+    private UserDataCache userDataCache;
+    @InjectMocks
+    private MainHandler mainHandler;
+
+    @Test
+    void startHandleUpdateTest() throws URISyntaxException, IOException {
+        String json = Files.readString(Path.of(
+                (TelegramBotUpdateListenerTest.class.getResource("update.json")).toURI()));
+        Update update = BotUtils.fromJson(json.replace("%text%", "/start"), Update.class);
+        ArgumentCaptor<SendMessage> argumentCaptorMessage = ArgumentCaptor.forClass(SendMessage.class);
+        SendMessage actual = argumentCaptorMessage.capture();
+        BotState botState = userDataCache.getUsersCurrentBotState(update.message().chat().id());
+        SendPhoto sendPhoto = new SendPhoto(update.message().chat().id(), new File("src/main/resources/mainMenu.png"));
+        sendPhoto.caption("""
+                    Вас приветствует приют для домашних животных.
+                    Наш приют содержит бездомных, покалеченных и больных животных (без учёта постоянно прибывающих) и
+                    очень нуждается в помощи и благотворительности на постоянной основе. Будем признательны за каждый
+                    вклад.
+                    Если вы неравнодушны, приезжайте общаться с хвостиками, привезите им вкусняшки, помогите копейкой, 
+                    им же немного нужно для счастья. А самое большое счастье для четвероногого друга - обрести свой 
+                    родной дом или чью-то родную душу.
+                    Мы очень будем рады любой помощи.
+                    """);
+        List<BaseRequest> baseRequestList = new ArrayList<>();
+        baseRequestList.add(sendPhoto);
+
+        when(botStateContext.processInputMessage(botState, update.message())).thenReturn(
+                new SendMessage());
+
+        Mockito.verify(botStateContext).processInputMessage(botState, update.message());
+        mainHandler.handleInputMessage(update.message());
+        Assertions.assertThat(actual.getParameters().get("chat_id")).isEqualTo(update.message().chat().id());
+        Assertions.assertThat(actual.getParameters().get("text")).isEqualTo("""
+                Вас приветствует приют для домашних животных.
+                Наш приют содержит бездомных, покалеченных и больных животных (без учёта постоянно прибывающих) и
+                очень нуждается в помощи и благотворительности на постоянной основе. Будем признательны за каждый
+                вклад.
+                Если вы неравнодушны, приезжайте общаться с хвостиками, привезите им вкусняшки, помогите копейкой, 
+                им же немного нужно для счастья. А самое большое счастье для четвероногого друга - обрести свой 
+                родной дом или чью-то родную душу.
+                Мы очень будем рады любой помощи.""");
+
+    }
+
+    @Test
+    void handleInputMessage() {
+    }
+
+    @Test
+    void handleQueryMessage() {
+    }
+}
---
+++ src/test/java/best/team/petprojectsquad/service/controllerServiceImpl/UserFeedBackControllerServiceImplTest.java
@@ -0,0 +1,63 @@
+package best.team.petprojectsquad.service.controllerServiceImpl;
+
+import best.team.petprojectsquad.entity.User;
+import best.team.petprojectsquad.entity.UserFeedBack;
+import best.team.petprojectsquad.repository.UserFeedBackRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserFeedBackControllerServiceImplTest {
+    @Mock
+    UserFeedBackRepository userFeedbackRepository;
+    @InjectMocks
+    UserFeedBackControllerServiceImpl userFeedBackControllerServiceImpl;
+    private long id = 0L;
+    UserFeedBack userFeedBack = new UserFeedBack();
+
+    @Test
+    void shouldReturnUserFeedBackById() {
+        when(userFeedbackRepository.getReferenceById(id)).thenReturn(userFeedBack);
+        UserFeedBack actual = userFeedBackControllerServiceImpl.getReferenceById(id);
+        Assertions.assertEquals(id, actual.getId());
+    }
+
+    @Test
+    void shouldReturnIdSavedUserFeedBack() {
+        when(userFeedbackRepository.save(userFeedBack)).thenReturn(userFeedBack);
+        long actual = userFeedBackControllerServiceImpl.save(userFeedBack);
+        Assertions.assertEquals(id, actual);
+    }
+
+    @Test
+    void deleteById() {
+//        Assertions.assertNull(userFeedBackControllerServiceImpl.deleteById(id));
+    }
+
+    @Test
+    void findAll() {
+        List<UserFeedBack> feedBackList = new ArrayList<>();
+        when(userFeedbackRepository.findAll()).thenReturn(feedBackList);
+        List<UserFeedBack> actual = userFeedBackControllerServiceImpl.findAll();
+        Assertions.assertEquals(feedBackList, actual);
+    }
+
+    @Test
+    void findById() {
+        Optional<UserFeedBack> optionalUserFeedBack = Optional.of(new UserFeedBack());
+        when(userFeedbackRepository.findById(id)).thenReturn(optionalUserFeedBack);
+        Optional<UserFeedBack> actual = userFeedBackControllerServiceImpl.findById(id);
+        Assertions.assertEquals(optionalUserFeedBack, actual);
+
+    }
+}
